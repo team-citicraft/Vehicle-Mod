02b0dcb2e8e6e9b209ee393c2efe0c74e9e6cf72
BuildTools <unconfigured@null.spigotmc.org>
2021-06-02 20:11:09 -0400

    removing the test thing

--- src/main/java/com/shadow/tbvm/VehicleMod.java
+++ src/main/java/com/shadow/tbvm/VehicleMod.java
@@ -13,7 +13,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-//test
 
 @Mod("tbvm")
 @Mod.EventBusSubscriber(modid = VehicleMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
